51924dbf3c047a751640a8e9ededd5d15f825eb5
Marcin Konowalczyk <marcin.konowalczyk@some.ox.ac.uk>
2019-07-08 12:05:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var items = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"];
 
 function test() {
-    // window.alert("dupa")
-    var item = items[Math.floor(Math.random()*items.length)];
-    document.getElementById("text").innerHTML = item
+    // window.alert("test")
+    var item = items[Math.floor(Math.random()*items.length)]; // Choose random text from the list
+    document.getElementById("text").innerHTML = item // Change the inner HTML of 'text' object to the chosen text
 }
